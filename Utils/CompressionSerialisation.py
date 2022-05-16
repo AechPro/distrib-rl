d669767c26318e3608ee2aabcccf8e63dfeb234f
Josh Lloyd <834342+nevercast@users.noreply.github.com>
2022-05-16 16:58:46 +1200

    Add minimum compression size

--- Utils/CompressionSerialisation.py
+++ Utils/CompressionSerialisation.py
@@ -3,6 +3,8 @@ import lz4.frame
 from msgpack_numpy import patch
 patch()
 
+MIN_SIZE_TO_COMPRESS = 1024
+
 LZ4 = True
 with_compression = False
 def set_compression(compression_to_use):
@@ -17,9 +19,11 @@ def decompress(data):
 
 def pack(data):
     data = msgpack.packb(data)
-    if with_compression:
+    compressed = False
+    if with_compression and MIN_SIZE_TO_COMPRESS <= len(data):
+        compressed = True
         data = compress(data)
-    return msgpack.packb((with_compression, data))
+    return msgpack.packb((compressed, data))
 
 def unpack(data):
     compressed, data = msgpack.unpackb(data)
