af432d036b6e26a0071b2a0de73918576eb4f8b1
Some Rando <103870616+some-rando-rl@users.noreply.github.com>
2022-05-16 23:06:25 +1200

    Add new obs with boost and demo timeouts

--- Environments/Custom/RocketLeague/ObsBuilderFactory.py
+++ Environments/Custom/RocketLeague/ObsBuilderFactory.py
@@ -2,6 +2,7 @@ from rlgym.utils.obs_builders import AdvancedObs, DefaultObs
 from rlgym_tools.extra_obs.advanced_padder import AdvancedObsPadder
 from rlgym_tools.extra_obs.advanced_stacker import AdvancedStacker
 from rlgym_tools.extra_obs.general_stacking import GeneralStacker
+from .ObsBuilders.DefaultWithTimeoutsObsBuilder import DefaultWithTimeoutsObsBuilder
 
 from Utils.FactoryBuilder import build_component_factory
 
@@ -10,12 +11,13 @@ _builders = {
     "advanced": AdvancedObs,
     "advanced_stacker": AdvancedStacker,
     "advanced_padder": AdvancedObsPadder,
+    "default_with_timeouts": DefaultWithTimeoutsObsBuilder,
     "general_stacker": GeneralStacker
 }
 
 _arg_transformers = { 
     "general_stacker": lambda **kwargs: {
-        "obs": build_obs_builder_from_config(kwargs), 
+        "obs": build_obs_builder_from_config(kwargs["obs"]), 
         "stack_size": kwargs.get("stack_size", 15)
     }
 }
