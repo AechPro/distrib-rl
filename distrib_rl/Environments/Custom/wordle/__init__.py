bc4f96a4585cb4095d05e56e73d8460952fc6910
Some Rando <103870616+some-rando-rl@users.noreply.github.com>
2022-08-09 23:45:17 +1200

    remove old unused custom envs

--- distrib_rl/Environments/Custom/wordle/__init__.py
+++
@@ -1,7 +0,0 @@
-from .wordle import *
-from .environment import Environment
-
-from gym import register
-register(
-    id='Wordle-v0',
-    entry_point='Environments.Custom.wordle.environment:Environment',)
